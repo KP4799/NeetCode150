dd75b898a331b7b3dd82acb148d58c8ff17384a5
Krutarth Patel <116948722+KP4799@users.noreply.github.com>
2026-01-22 22:01:50 +0530

    Day 5

--- Arrays&Hashing/Top K Frequent.py
+++ Arrays&Hashing/Top K Frequent.py
@@ -1,3 +1,19 @@
+"""
+Problem: LeetCode 347 - Top K Frequent Elements
+
+Key Idea:
+To find the k most frequent elements in the given list, we can use a hash map (dictionary in Python) to keep track of each element.
+We assign the elements to a list represented by the same element (key). Then we sort the hash map in reverse order based on the length of these lists.
+The top K keys return the most frequent items.
+
+Time Complexity:
+The time complexity of this approach is O(n + k*log(n)), where n is the number of elements in the input list. Building the frequency map takes O(n) time, and inserting k elements into the min-heap takes O(k*log(n)) time.
+
+Space Complexity:
+The space complexity is O(n) because we need to store the frequency map of all elements in the input list. Additionally, the min-heap will also have a space complexity of O(n) in the worst case if all elements are unique.
+"""
+
+
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         hashmap = {}
