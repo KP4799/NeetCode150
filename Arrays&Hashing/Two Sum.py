66b1a03c3c72d227e894e1b90906f6b5a05435b6
Krutarth Patel <116948722+KP4799@users.noreply.github.com>
2026-01-24 02:06:02 +0530

    Day 3

--- Arrays&Hashing/Two Sum.py
+++ Arrays&Hashing/Two Sum.py
@@ -10,6 +10,7 @@ The time complexity of this solution is O(n), where n is the size of the input a
 Space Complexity:
 The space complexity is O(n), as the hash map can potentially store all elements of the input array 'nums' if they are all unique. In the best case, where the target sum is achieved with the first two elements, the space complexity would be O(1). However, in the worst case, if all elements are distinct, the hash map will store all elements, leading to O(n) space complexity.
 """
+
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         prevMap = {}
