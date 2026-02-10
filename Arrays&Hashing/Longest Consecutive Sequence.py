b459fc931755e7cda6f2f2faf0bd98d5f6e47860
Krutarth Patel <116948722+KP4799@users.noreply.github.com>
2026-02-10 23:26:43 +0530

    Update Longest Consecutive Sequence.py

--- Arrays&Hashing/Longest Consecutive Sequence.py
+++ Arrays&Hashing/Longest Consecutive Sequence.py
@@ -1,3 +1,16 @@
+"""
+Problem: LeetCode 128 -Longest Consecutive Sequence
+
+Key Idea:
+To find the longest consecutive subsequence, we create a dictionary which holds the current number as the key and the maximum length of sequence possible from that number as the value. We then return the maximum value of all to give the largest consecutive sequence.
+
+Time Complexity:
+The time complexity of this solution is O(n), where n is the number of elements in the input array 'nums'. The dictionary operations (addition and lookup) take O(1) time on average, and we perform these operations for each element once.
+
+Space Complexity:
+The space complexity is O(n), where n is the number of elements in the input array 'nums'. In the worst case, the set will store all elements from the input array if they are all distinct.
+"""
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         count = 1
