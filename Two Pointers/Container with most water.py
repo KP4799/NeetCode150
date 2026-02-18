3b29d6dbcf72b0d136973d40c56fb7daca862156
Krutarth Patel <116948722+KP4799@users.noreply.github.com>
2026-02-18 11:28:58 +0530

    Create Container with most water.py

---
+++ Two Pointers/Container with most water.py
@@ -0,0 +1,52 @@
+"""
+Problem: LeetCode 11 - Container With Most Water
+
+Key Idea:
+To find the maximum area of water that can be held between two vertical lines, we can use a two-pointer approach. We initialize two pointers, one at the beginning of the input array (i) 
+and the other at the end of the array (j). The area between the two vertical lines is calculated as the minimum of the heights at the two pointers multiplied by the distance between them.
+We then update the maximum area found so far and move the pointer with the smaller height towards the other pointer. We continue this process until the two pointers meet or cross each other.
+
+Time Complexity:
+The time complexity of this solution is O(n), where n is the number of elements in the input array 'height'. The two-pointer approach iterates through the array once, and at each step, 
+we move at least one of the pointers, so we do not revisit any element.
+
+Space Complexity:
+The space complexity is O(1) since we are not using any additional data structures that depend on the input size. We only use a constant amount of extra space for the two pointers and other variables.
+"""
+
+
+# Two pointer
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        i, j = 0, len(heights) - 1
+        max_area = 0
+
+        while i < j:
+            current_area = min(heights[i], heights[j]) * (j - i)
+            max_area = max(max_area, current_area)
+
+            if heights[i] < heights[j]:
+                i += 1
+            else:
+                j -= 1
+
+        return max_area
+
+
+# Brute Force
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_area = 0
+        for i in range(len(heights)):
+            j = i + 1
+            
+            while j < len(heights):
+                current_area = min(heights[i],heights[j]) * (j-i)
+                max_area = max(max_area,current_area)
+                j += 1
+        
+        return max_area
+
+# x = Solution()
+# y = x.maxArea([1,7,2,5,4,7,3,6])
+# print(y)
